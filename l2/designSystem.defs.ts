77fffb7cd823b99f34fe1026eb5f46c0f15dead0
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-05-21 14:10:40 -0300

    group-1
    group-1

--- l2/designSystem.defs.ts
+++
@@ -1,2 +0,0 @@
-/// <mls fileReference="_102035_/l2/designSystem.defs.ts" enhancement="_blank"/>
-
--- l2/project.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls fileReference="_102035_/l2/project.test.ts" enhancement="_blank"/>
-
- import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
