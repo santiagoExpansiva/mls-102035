5e6d32a04b157bf896c6f69b7d87e792021551ea
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-05-27 17:42:21 -0300

    group-1
    group-1

---
+++ l2/pizzaria/index.defs.ts
@@ -0,0 +1,2 @@
+/// <mls fileReference="_102035_/l2/pizzaria/index.defs.ts" enhancement="_blank"/>
+
--- l2/pizzaria/module.ts
+++ l2/pizzaria/module.ts
@@ -11,7 +11,7 @@ export const moduleGenome: Record<string, IGenomeConfig> = {
   
 export const skills: IPaths = {
   web: {
-    sharedPath: '/_102020_/l2/pizzaria/web/shared',
+    sharedPath: '/_102035_/l2/pizzaria/web/shared',
     sharedSkill: '/_102020_/l2/agents/newModule/skills/genPageShared.ts'
   }
 }
@@ -32,7 +32,20 @@ export const moduleFrontendDefinition: AuraModuleFrontendDefinition = {
   pageTitle: 'pizzaria',
   device: 'desktop',
   navigation: [
+    {
+      id: 'cashRegister',
+      label: 'cashRegister',
+      href: '/pizzaria/cashRegister',
+      description: 'cashRegister',
+    },
   ],
   routes: [
+    {
+      path: '/pizzaria/cashRegister',
+      aliases: [],
+      entrypoint: '/_102035_/l2/pizzaria/web/desktop/page11/cashRegister.js',
+      tag: 'pizzaria--web--desktop--page11--cash-register-102035',
+      title: 'cashRegister',
+    },
   ],
 };
